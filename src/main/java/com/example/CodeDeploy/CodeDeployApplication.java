607819391478d75a3dfbd07cfff7b330f3bfbf0d
Sajal Jain <sjain@sebonetechnologies.com>
2022-09-06 23:33:29 +0530

    first commit

---
+++ src/main/java/com/example/CodeDeploy/CodeDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.CodeDeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeDeployApplication.class, args);
+	}
+
+}
